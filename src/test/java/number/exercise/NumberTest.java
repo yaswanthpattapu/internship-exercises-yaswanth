ebc554fe2f4af8703acc153d4c9c47acd26aaff2
Yaswanth Kumar Pattapu <yaswanthpyk090@gmail.com>
2022-04-04 21:28:21 +0530

    Update NumberTest.java

--- src/test/java/number/exercise/NumberTest.java
+++ src/test/java/number/exercise/NumberTest.java
@@ -13,13 +13,7 @@ public class NumberTest {
         Number inp = new Number();
         List<String> list1 = inp.range(1,15);
         List<String> list2 = Arrays.asList("Odd", "2", "3", "Even", "5", "Even", "7", "Even", "Odd", "Even", "11", "Even", "13", "Even", "Odd");
-        System.out.println(list1);
-        System.out.println(list2);
-
-//        for(int i=0; i<list1.size();i++)
-//        {
-//            assertThat(list1.get(i),is(list1.get(i)));
-//        }
+   
         Assert.assertEquals(list1,list2);
     }
 }
