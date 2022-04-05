cf819696d75163133fa9c801a35a7a40d8052537
yaswanthkkumarpattapu <yaswanthpyk090@gmail.com>
2022-04-05 09:52:36 +0530

    TestChanges

--- src/test/java/number/exercise/NumberTest.java
+++ src/test/java/number/exercise/NumberTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 public class NumberTest {
 
     @Test
@@ -12,14 +15,21 @@ public class NumberTest {
     {
         Number inp = new Number();
         List<String> list1 = inp.range(1,15);
-        List<String> list2 = Arrays.asList("Odd", "2", "3", "Even", "5", "Even", "7", "Even", "Odd", "Even", "11", "Even", "13", "Even", "Odd");
-        System.out.println(list1);
-        System.out.println(list2);
+        List<String> list2 = Arrays.asList("Odd", "2", "3", "Even", "5", "Even", "7", "Even", "Odd", "Even", "11",
+                "Even", "13", "Even", "Odd");
 
-//        for(int i=0; i<list1.size();i++)
-//        {
-//            assertThat(list1.get(i),is(list1.get(i)));
-//        }
-        Assert.assertEquals(list1,list2);
+        for(int i=0;i<list1.size();i++)
+        {
+            Assert.assertThat(list1.get(i),is(list2.get(i)));
+        }
+
+        List<String> list3 = inp.range(123,145);
+        List<String> list4 = Arrays.asList("Odd", "Even", "Odd", "Even", "127", "Even", "Odd", "Even", "131", "Even",
+                "Odd", "Even", "Odd", "Even", "137", "Even", "139", "Even", "Odd", "Even", "Odd", "Even", "Odd");
+
+        for(int i=0;i<list3.size();i++)
+        {
+            Assert.assertThat(list3.get(i),is(list4.get(i)));
+        }
     }
 }
